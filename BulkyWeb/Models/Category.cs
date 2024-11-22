18e049c0e1de54515c8b320d827f980aaff1f36d
Eric Kim <deveric89@naver.com>
2024-11-23 00:19:33 +0900

    DB Connected Table Created

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models {
+    public class Category {
+        [Key]
+        public int Id { get; set; } // Primary Key on DB
+        [Required]
+        public string Name { get; set; } // This field will be set to not null option in SQL
+        public int DisplayOrder { get; set; }
+    }
+}
