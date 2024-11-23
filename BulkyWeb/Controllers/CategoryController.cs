3e7f1184532604593dd28c4354b4434a8f84aade
Eric Kim <deveric89@naver.com>
2024-11-24 00:22:33 +0900

    Add Buttons Edit Delete

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,5 +18,35 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "DisplayOrder must different with a Category Name");
+            }
+
+            if (obj.Name != null && obj.Name == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                //Redirect to main page of Category
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models {
     public class Category {
         [Key]
         public int Id { get; set; } // Primary Key on DB
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; } // This field will be set to not null option in SQL
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Please enter the number between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
